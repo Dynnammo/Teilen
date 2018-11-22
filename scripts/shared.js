3998b1f317d200f0d07712ca3c4cac232a88201c
tempteamcode <38981204+tempteamcode@users.noreply.github.com>
2018-11-22 13:38:39 +0100

    Add "account created" and "changes saved" messages

--- scripts/shared.js
+++ scripts/shared.js
@@ -12,3 +12,48 @@ function ready(action) {
   document.addEventListener("DOMContentLoaded", action);
 }
 
+
+ready(
+function () {
+    var msg = getUrlParameter("msg");
+    
+    var message = "";
+    switch (msg) {
+        case "new":
+            message = "Vous allez recevoir un email avec un lien pour confirmer la création de votre compte.";
+        break;
+        case "edit":
+            message = "Les modifications apportées ont été enregistrées !";
+        break;
+    }
+    
+    if (message.length > 0) // alert(message);
+    {
+        addStyle("https://jqueryui.com/resources/demos/style.css");
+        addStyle("https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css");
+        addScript("https://code.jquery.com/ui/1.12.1/jquery-ui.js");
+        setTimeout(function() {
+            var dialog = document.createElement("div");
+            dialog.id = "dialog";
+            dialog.title = "Information";
+            dialog.innerText = message;
+            document.body.appendChild(dialog);
+            $("#dialog").dialog();
+        }, 500);
+    }
+}
+);
+
+function addStyle(src) {
+    var style = document.createElement("link");
+    style.rel = "stylesheet";
+    style.type = "text/css";
+    style.href = src;
+    document.head.appendChild(style);
+}
+
+function addScript(src) {
+    var script = document.createElement("script");
+    script.src = src;
+    document.head.appendChild(script);
+}
